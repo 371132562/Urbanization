327dc0aacee977019df670f21fcf2d0a9cf52449
Star <371132562@qq.com>
2025-07-17 18:26:06 +0800

    Revert "feat: 更新项目配置和构建流程"
    This reverts commit ef5a1532f2776b6d285a9386011c8a6c33514a70.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
+import { UPLOAD_DIR } from './utils/file-upload.utils'; // 导入上传目录常量
 
 //业务模块
 import { DataManagementModule } from './businessComponent/dataManagement/dataManagement.module';
@@ -12,30 +13,22 @@ import { IndicatorModule } from './businessComponent/indicator/indicator.module'
 import { CountryAndContinentModule } from './businessComponent/countryAndContinent/countryAndContinent.module';
 import { ArticleModule } from './businessComponent/article/article.module';
 
-// 判断是否为生产环境（在 Electron 中打包后，NODE_ENV 会被设置成 'production'）
-const isProd = process.env.NODE_ENV === 'production';
-
 @Module({
   imports: [
     //公共模块
     // 配置 @nestjs/serve-static 模块来提供静态文件服务
     ServeStaticModule.forRoot(
       {
-        rootPath: join(process.cwd(), process.env.UPLOAD_DIR as string), // 静态文件在服务器上的物理路径
-        serveRoot: `/${(process.env.UPLOAD_DIR as string).replace('./', '')}`, // URL 前缀，例如 /uploads/images
+        rootPath: join(process.cwd(), UPLOAD_DIR), // 静态文件在服务器上的物理路径
+        serveRoot: `/${UPLOAD_DIR.replace('./', '')}`, // URL 前缀，例如 /uploads/images
         // serveRoot: '/', // 可以省略，默认就是 '/'
         exclude: ['/'], // 可选：排除不需要提供静态服务的路由
       },
       {
-        // 根据环境动态设置前端静态资源的根路径
-        // 在生产环境中，__dirname 是 .../resources/backend/dist，
-        // 所以我们需要向上回溯两层到 resources 目录，再进入 'frontend-dist'
-        // 在开发环境中，我们直接使用 process.cwd() 定位到 monorepo 根目录
-        rootPath: isProd
-          ? join(__dirname, '..', '..', 'frontend-dist')
-          : join(process.cwd(), 'frontend', 'dist'),
+        rootPath: join(process.cwd(), 'frontend', 'dist'), // 指向 monorepo 根目录下的 frontend/dist
+        // serveRoot: '/', // 可以省略，默认就是 '/'
         serveStaticOptions: {
-          preCompressed: true, // 如果前端构建时生成了 .gz 文件，可以开启以提升性能
+          preCompressed: true,
         },
       },
     ),
